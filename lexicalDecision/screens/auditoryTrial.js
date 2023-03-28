1e1dd7fec9afaea6677bc6cf3b4af381c23b4480
Jonathan Slawitsky <jonathanslawitsky@Jonathans-Air.askey.com>
2023-03-28 14:38:29 -0400

    Fix auditory

--- lexicalDecision/screens/auditoryTrial.js
+++ lexicalDecision/screens/auditoryTrial.js
@@ -6,7 +6,7 @@ import { AUDIO_CONTAINER, AUDIO_SOURCE } from "../components/audioContainer"
 
 class AuditoryTrialScreen extends LexicalDecisionTrialScreen {
     components = new Map([
-        [TEXT_CONTAINER, {text: '+', css: {color: '#000000'}}],
+        [TEXT_CONTAINER, {text: '+', css: {color: '#000000', height: '90px'}}],
         [AUDIO_CONTAINER, {}],
         [BUTTON_CONTAINER, {}],
         [BUTTON_LABEL_CONTAINER, {}]
@@ -22,12 +22,14 @@ class AuditoryTrialScreen extends LexicalDecisionTrialScreen {
         this.task.newTrial()
         super.render()
         AUDIO_SOURCE.attr('src', this.task.currentTrial.audioSource)
+        AUDIO_CONTAINER.off('ended')
         AUDIO_CONTAINER.on('ended', () => {
-            this.task.inTrial = true
             this.task.currentTrial.startTime = new Date()
+            this.task.inTrial = true
             this.updateText('')
             this.timeoutID = setTimeout(() => this.responseClickHandler(null), 5000)
         })
+        AUDIO_CONTAINER[0].load()
         AUDIO_CONTAINER[0].play()
     }
 }
