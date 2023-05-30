782456266f2820076d5bf8d2903faf89fc09753b
Jonathan Slawitsky <jonathan.slawitsky@gmail.com>
2023-05-30 14:18:35 -0400

    Update columns in lex

--- lexicalDecision/task.js
+++ lexicalDecision/task.js
@@ -1,12 +1,12 @@
 import { InputDevicesScreen } from "../shared/screens/inputDevices"
 import { InstructionsOne, InstructionsThree, InstructionsTwo } from "./screens/instructions"
-import { BeginOrPracticeAgain, Break, Finished, Incorrect, TimeOut } from "../shared/screens/transitions"
 import { AuditoryTrialScreen } from "./screens/auditoryTrial"
 import { WrittenTrialScreen } from "./screens/writtenTrial"
 import { ParticipantIdScreen } from "../shared/screens/participantID"
 import { Trial } from "./trial"
 import { BaseTask } from "../shared/task"
 import { GREEN_LABEL, RED_LABEL } from "../shared/components/responseButtons"
+import { QualtricsClient } from "../shared/qualtricsClient"
 
 
 class Task extends BaseTask {
@@ -58,57 +58,27 @@ class Task extends BaseTask {
     }
 
     submit() {
-        const columns = {
-            'ItemNum': [],
-            'Item': [],
-            'CRESP': [],
-            'TimedOut': [],
-            'RT': [],
-            'Accuracy': [],
-            'WordType': [],
-            'Frequency': [],
-            'Regularity': [],
-            'Imageability': [],
-            'PartofSpeech': [],
-            'Lexicality': [],
-            'Response': [],
-            'TrialType': [],
-            'Time': []
-        }
-        const mouseMoveDistances = []
-        const mouseMoveDurations = []
-        const mouseMoveAverageVelocities = []
-        const firstMouseMoves = []
-
-        this.trials.map((trial) => {
-            let firstMouseMove, duration, distance, avgVelocity 
-            [firstMouseMove, duration, distance, avgVelocity] = trial.computeMousemoveStats()
-            firstMouseMoves.push(firstMouseMove)
-            mouseMoveDurations.push(duration)
-            mouseMoveDistances.push(distance)
-            mouseMoveAverageVelocities.push(avgVelocity)
-            
-            for (const [key, values] of Object.entries(columns)) {
-                values.push(trial[key])
-            }
-        })
-
+        const columns = [
+            'ItemNum',
+            'Item',
+            'CRESP',
+            'TimedOut',
+            'RT',
+            'Accuracy',
+            'WordType',
+            'Frequency',
+            'Regularity',
+            'Imageability',
+            'PartofSpeech',
+            'Lexicality',
+            'Response',
+            'TrialType',
+            'Time'
+        ]
         if (window.location.host === "georgetown.az1.qualtrics.com") {
-            for (const [key, values] of Object.entries(columns)) {
-                Qualtrics.SurveyEngine.setEmbeddedData(key, values.join(';'))
-            }
-            Qualtrics.SurveyEngine.setEmbeddedData('userAgent', navigator.userAgent.replace(',', '|').replace(';','|'))
-            Qualtrics.SurveyEngine.setEmbeddedData('inputDevice', this.inputDevice)
-            Qualtrics.SurveyEngine.setEmbeddedData('SubjectID', this.participantID)
-            Qualtrics.SurveyEngine.setEmbeddedData('firstMouseMoves', firstMouseMoves.join(';'))
-            Qualtrics.SurveyEngine.setEmbeddedData('mouseMoveDurations', mouseMoveDurations.join(';'))
-            Qualtrics.SurveyEngine.setEmbeddedData('mouseMoveDistances', mouseMoveDistances.join(';'))
-            Qualtrics.SurveyEngine.setEmbeddedData('mouseMoveAverageVelocities', mouseMoveAverageVelocities.join(';'))
-            Qualtrics.SurveyEngine.setEmbeddedData('RecipientFirstName', 'N/A')
-            Qualtrics.SurveyEngine.setEmbeddedData('RecipientLastName', 'N/A')
-            Qualtrics.SurveyEngine.setEmbeddedData('RecipientEmail', 'N/A')
-            Qualtrics.SurveyEngine.setEmbeddedData('ExternalReference', 'N/A')
-
+            const qualtricsClient = new QualtricsClient(columns, this.inputDevice, this.participantID)
+            qualtricsClient.collectTrialData(this.trials)
+            qualtricsClient.submitEmbeddedData()
             this.engine.clickNextButton()
         }
     }
