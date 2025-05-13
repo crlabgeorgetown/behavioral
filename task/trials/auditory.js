c887033dd95600a061fc582150662639e1aa4886
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2025-05-12 20:15:24 -0700

    Trial class for Auditory Syllable to Grapheme Matching

--- task/trials/auditory.js
+++ task/trials/auditory.js
@@ -68,4 +68,70 @@ class AuditoryLetterIDTrial {
     }
 }
 
-export { AuditoryLetterIDTrial }
+class AuditorySyllableToGraphemeMatchingTrial {
+    constructor(config) {
+        this.ItemNum = config.ItemNum
+        this.TrialType = config.TrialType
+
+        this.topleft = config.topleft
+        this.topright = config.topright
+        this.botleft = config.botleft
+        this.botright = config.botright
+
+        this.location = {
+            ['topleft']: config.topleft,
+            ['topright']: config.topright,
+            ['botleft']: config.botleft,
+            ['botright']: config.botright
+        }
+
+        this.CRESP = config.CRESP
+        this.TargetLocation = config.TargetLocation
+
+        this.Response = ''
+        this.ResponseLocation = ''
+
+        this.startTime = null
+        this.responseTime = null
+        this.TimedOut = null
+        this.TrialWasAdministered = 1
+    }
+
+    isCorrect() {
+        return this.ResponseLocation === this.TargetLocation
+    }
+
+    get columns() {
+        return [
+            ...Object.keys(this),
+            'Accuracy',
+            'Time',
+            'Date',
+            'TimeOnItem',
+            'TimedOut',
+            'TrialWasAdministered'
+        ]
+    }
+
+    get Accuracy() {
+        return this.isCorrect() ? 1 : 0
+    }
+
+    get TimeOnItem() {
+        return this.responseTime - this.startTime
+    }
+    get Time() {
+        return this.startTime.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', second: '2-digit', hourCycle: 'h24'})
+    }
+
+    get Date() {
+        return this.startTime.toLocaleTimeString('en-US')
+    }
+
+    audioSource() {
+        const sound = this.CRESP
+        return `https://3m1l1032.github.io/behavioral/static/auditory_syllable_to_grapheme_matching_wav_files/${sound}.wav`
+    }
+}
+
+export { AuditoryLetterIDTrial, AuditorySyllableToGraphemeMatchingTrial }
