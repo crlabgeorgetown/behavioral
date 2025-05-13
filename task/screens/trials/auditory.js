6e5284a254a5077d9d8fdfabea902a63b6752560
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2025-05-12 20:29:38 -0700

    Screen for auditory syllable to grapheme matching

--- task/screens/trials/auditory.js
+++ task/screens/trials/auditory.js
@@ -68,6 +68,66 @@ class AuditoryLetterIDTrialScreen extends Screen {
     }
 }
 
+class AuditorySyllableToGraphemeMatchingTrialScreen extends Screen {
+    get components() {
+        return new Map([
+            [TEXT_CONTAINER, {text: '+', addClass: 'base-text extra-large-text large-fixed-height'}],
+            [AUDIO_CONTAINER, {}],
+            [FOUR_LETTER_CONTAINER, {addClass: 'grid-container four-letter-container'}],
+        ])
+    }
+
+    get clickHandlers() {
+        return { 
+            topleft: (event) => this.proceedClickHandler(event, 'topleft'),
+            botleft: (event) => this.proceedClickHandler(event, 'botleft'),
+            botright: (event) => this.proceedClickHandler(event, 'botright'),
+            topright: (event) => this.proceedClickHandler(event, 'topright')
+        }
+    }
+
+    proceedClickHandler(event, location) {
+        event.stopPropagation()
+        clearTimeout(this.timeoutID)
+
+        this.orchestrator.currentTrial.responseTime = new Date()
+        this.orchestrator.currentTrial.ResponseLocation = location
+        this.orchestrator.currentTrial.Response = this.orchestrator.currentTrial.location[location]
+        const isPractice = this.orchestrator.currentTrial.TrialType === 'Practice'
+
+        if (!this.orchestrator.currentTrial.isCorrect() && isPractice) {
+            this.orchestrator.replay()
+        } else {
+            this.orchestrator.next()
+        }
+        TEXT_CONTAINER.show()
+    }
+
+    startTrial() {
+        FOUR_LETTER_CONTAINER.hide()
+        topleft.text(this.orchestrator.currentTrial.topleft)
+        botleft.text(this.orchestrator.currentTrial.botleft)
+        botright.text(this.orchestrator.currentTrial.botright)
+        topright.text(this.orchestrator.currentTrial.topright)
+        setTimeout(() => {
+            AUDIO_SOURCE.attr('src', this.orchestrator.currentTrial.audioSource())
+            AUDIO_CONTAINER[0].load()
+            TEXT_CONTAINER.hide()
+            FOUR_LETTER_CONTAINER.show()
+            setTimeout(() => {
+                AUDIO_CONTAINER[0].play()
+                this.orchestrator.currentTrial.startTime = new Date()
+                this.timeoutID = setTimeout(() => {
+                    this.orchestrator.currentTrial.TimedOut = true
+                    this.orchestrator.currentTrial.responseTime = new Date()
+                    this.orchestrator.timedOut()
+                    TEXT_CONTAINER.show()
+                }, this.orchestrator.variant.timeToTimeout)
+            }, this.orchestrator.variant.waitDuration)
+        }, this.orchestrator.variant.fixationDuration)
+    }
+}
+
 class InstructionAuditoryLetterID extends Screen {
     get components() {
         topleft.text('d')
@@ -113,4 +173,4 @@ class InstructionAuditorySyllableToGraphemeMatching extends Screen {
     }
 }
 
-export { InstructionAuditoryLetterID, AuditoryLetterIDTrialScreen, InstructionAuditorySyllableToGraphemeMatching }
+export { InstructionAuditoryLetterID, AuditoryLetterIDTrialScreen, InstructionAuditorySyllableToGraphemeMatching, AuditorySyllableToGraphemeMatchingTrialScreen }
