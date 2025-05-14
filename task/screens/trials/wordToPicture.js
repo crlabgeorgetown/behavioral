8141bb36b1cc15bf9289978bf59bbf4e3b9e2271
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2025-05-13 21:49:35 -0700

    adding trial class screen and custom instruction screen for auditory word to picture

---
+++ task/screens/trials/wordToPicture.js
@@ -0,0 +1,92 @@
+import Screen from "../base";
+import { TEXT_CONTAINER } from "../../../shared/components/textContainer";
+import { AUDIO_CONTAINER, AUDIO_SOURCE } from "../../../shared/components/audioContainer";
+import { INSTRUCTION_BUTTON_CONTAINER } from "../../../shared/components/instructionButtons";
+import { FOUR_IMAGE_CONTAINER, topleftImage, toprightImage, botleftImage, botrightImage } from "../../../shared/components/imageContainer";
+
+class AuditoryWordToPictureMatchingReadMapTrialScreen extends Screen {
+    get components() {
+        return new Map([
+            [FOUR_IMAGE_CONTAINER, {addClass: 'four-image-container'}],
+            [TEXT_CONTAINER, {text: '+', addClass: 'base-text extra-large-text large-fixed-height'}],
+            [AUDIO_CONTAINER, {}]
+        ])
+    }
+
+    get clickHandlers() {
+        return { 
+            topleftImage: (event) => this.proceedClickHandler(event, 'topleft'),
+            toprightImage: (event) => this.proceedClickHandler(event, 'topright'),
+            botleftImage: (event) => this.proceedClickHandler(event, 'botleft'),
+            botrightImage: (event) => this.proceedClickHandler(event, 'botright')
+        }
+    }
+
+    proceedClickHandler(event, location) {
+        event.stopPropagation()
+        clearTimeout(this.timeoutID)
+
+        this.orchestrator.currentTrial.responseTime = new Date()
+        this.orchestrator.currentTrial.ResponseLocation = location
+        this.orchestrator.currentTrial.Response = this.orchestrator.currentTrial.location[location]
+        const isPractice = this.orchestrator.currentTrial.TrialType === 'Practice'
+
+        if (!this.orchestrator.currentTrial.isCorrect() && isPractice) {
+            this.orchestrator.replay()
+        } else {
+            this.orchestrator.next()
+        }
+
+        TEXT_CONTAINER.show()
+    }
+
+    startTrial() {
+        // prepare the trial
+        // load the pictures into the containers
+        FOUR_IMAGE_CONTAINER.hide()
+        topleftImage.attr('src', this.orchestrator.currentTrial.getTopLeft())
+        toprightImage.attr('src', this.orchestrator.currentTrial.getTopRight())
+        botleftImage.attr('src', this.orchestrator.currentTrial.getBotLeft())
+        botrightImage.attr('src', this.orchestrator.currentTrial.getBotRight())
+
+        setTimeout(() => {
+            AUDIO_SOURCE.attr('src', this.orchestrator.currentTrial.audioSource())
+            AUDIO_CONTAINER[0].load()
+            AUDIO_CONTAINER[0].play()
+            setTimeout(() => {
+                TEXT_CONTAINER.hide()
+                FOUR_IMAGE_CONTAINER.show()
+                this.orchestrator.currentTrial.startTime = new Date()
+                this.timeoutID = setTimeout(() => {
+                    this.orchestrator.currentTrial.TimedOut = true
+                    this.orchestrator.currentTrial.responseTime = new Date()
+                    this.orchestrator.timedOut()
+                    TEXT_CONTAINER.show()
+                }, this.orchestrator.variant.timeToTimeout)
+            }, this.orchestrator.variant.waitDuration)
+        }, this.orchestrator.variant.fixationDuration)
+    }
+}
+
+class InstructionAuditoryWordToPictureMatching extends Screen {
+    get components() {
+        topleftImage.attr('src', 'https://crlabgeorgetown.github.io/behavioral/static/auditoryWordToPictureMatching/swim.jpeg')
+        toprightImage.attr('src', 'https://crlabgeorgetown.github.io/behavioral/static/auditoryWordToPictureMatching/run.jpeg')
+        botleftImage.attr('src', 'https://crlabgeorgetown.github.io/behavioral/static/auditoryWordToPictureMatching/dance.jpeg')
+        botrightImage.attr('src', 'https://crlabgeorgetown.github.io/behavioral/static/auditoryWordToPictureMatching/climb.jpeg')
+        return new Map([
+            [FOUR_IMAGE_CONTAINER, {addClass: 'four-image-container-instruction'}],
+            [TEXT_CONTAINER, {text: 'You will hear a word.\nTouch the picture that matches the word.', addClass: 'base-text medium-text'}],
+            [INSTRUCTION_BUTTON_CONTAINER, {}]
+        ])
+    }
+
+    get clickHandlers() {
+        return { 
+            nextButton: (event) => this.orchestrator.next(),
+            previousButton: (event) => this.orchestrator.previous()
+        }
+    }
+}
+
+export { InstructionAuditoryWordToPictureMatching, AuditoryWordToPictureMatchingReadMapTrialScreen }
