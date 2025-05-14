23b2714bdf19c95ab021b2ddb3ea2585f2423cad
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2025-05-13 21:50:13 -0700

    adding formatting and layouts for image container (choices)

--- shared/components/imageContainer.js
+++ shared/components/imageContainer.js
@@ -1,4 +1,30 @@
 const IMAGE_CONTAINER = jQuery('<img/>', {id: 'stop', class: 'stop'})
 
+const FOUR_IMAGE_CONTAINER = jQuery('<div/>', {
+    id: 'four-image-container', 
+    class: 'four-image-container'
+})
 
-export { IMAGE_CONTAINER }
+const topleftImage = jQuery('<img/>', {
+    id: 'topleftImage',
+    class: 'image-container'
+})
+
+const toprightImage = jQuery('<img/>', {
+    id: 'toprightImage',
+    class: 'image-container'
+})
+
+const botleftImage = jQuery('<img/>', {
+    id: 'botleftImage',
+    class: 'image-container'
+})
+
+const botrightImage = jQuery('<img/>', {
+    id: 'botrightImage',
+    class: 'image-container'
+})
+
+FOUR_IMAGE_CONTAINER.append(topleftImage, toprightImage, botleftImage, botrightImage)
+
+export { IMAGE_CONTAINER, FOUR_IMAGE_CONTAINER, topleftImage, toprightImage, botleftImage, botrightImage }
