1d82d269078cf010f803b549160194d951c98c61
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2025-05-14 23:54:35 -0700

    adding variant trial class for arizona semantic test

---
+++ task/trials/arizonaSemanticTest.js
@@ -0,0 +1,88 @@
+class ArizonaSemanticTestTrial {
+    constructor(config) {
+        this.ItemNum = config.ItemNum
+        this.TrialType = config.TrialType
+        this.Procedure = config.Procedure
+
+        this.topleft = config.topleft
+        this.topright = config.topright
+        this.bottomleft = config.bottomleft
+        this.bottomright = config.bottomright
+
+        this.location = {
+            ['topleft']: config.topleft,
+            ['topright']: config.topright,
+            ['bottomleft']: config.bottomleft,
+            ['bottomright']: config.bottomright
+        }
+
+        this.targetimage = config.targetimage
+        this.CRESP = config.CRESP
+        this.TargetLocation = config.TargetLocation
+
+        this.Response = ''
+        this.ResponseLocation = ''
+
+        this.startTime = null
+        this.responseTime = null
+        this.TimedOut = null
+        this.TrialWasAdministered = 1
+    }
+
+    isCorrect() {
+        return this.ResponseLocation === this.TargetLocation
+    }
+
+    get columns() {
+        return [
+            ...Object.keys(this),
+            'Accuracy',
+            'Time',
+            'Date',
+            'TimedOut',
+            'RT'
+        ]
+    }
+
+    get Accuracy() {
+        return this.isCorrect() ? 1 : 0
+    }
+    get RT() {
+        return this.responseTime - this.startTime
+    }
+
+    get Time() {
+        return this.startTime.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', second: '2-digit', hourCycle: 'h24'})
+    }
+
+    get Date() {
+        return this.startTime.toLocaleDateString('en-US')
+    }
+
+    getTargetImage() {
+        const targetimage = this.targetimage
+        return `https://crlabgeorgetown.github.io/behavioral/static/arizonaSemanticTest/${targetimage}.png`
+    }
+
+    getTopLeft() {
+        const topleft = this.topleft
+        return `https://crlabgeorgetown.github.io/behavioral/static/arizonaSemanticTest/${topleft}.png`
+    }
+
+    getTopRight() {
+        const topright = this.topright
+        return `https://crlabgeorgetown.github.io/behavioral/static/arizonaSemanticTest/${topright}.png`
+    }
+
+    getBottomLeft() {
+        const bottomleft = this.bottomleft
+        return `https://crlabgeorgetown.github.io/behavioral/static/arizonaSemanticTest/${bottomleft}.png`
+    }
+
+    getBottomRight() {
+        const bottomright = this.bottomright
+        return `https://crlabgeorgetown.github.io/behavioral/static/arizonaSemanticTest/${bottomright}.png`
+    }
+}
+
+export { ArizonaSemanticTestTrial }
