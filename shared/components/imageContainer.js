54543418395767edb2cf9ffbf955b614ca6c1822
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2025-05-15 17:41:12 -0700

    Creating image containers for the Arizona Semantic Test

--- shared/components/imageContainer.js
+++ shared/components/imageContainer.js
@@ -1,5 +1,6 @@
 const IMAGE_CONTAINER = jQuery('<img/>', {id: 'stop', class: 'stop'})
 
+// Word to Picture Matching
 const FOUR_IMAGE_CONTAINER = jQuery('<div/>', {
     id: 'four-image-container', 
     class: 'four-image-container'
@@ -34,4 +35,38 @@ const TEXT_CRESP_CONTAINER = jQuery('<div/>', {
 
 FOUR_IMAGE_CONTAINER.append(topleftImage, toprightImage, botleftImage, botrightImage)
 
-export { IMAGE_CONTAINER, FOUR_IMAGE_CONTAINER, topleftImage, toprightImage, botleftImage, botrightImage, TEXT_CRESP_CONTAINER }
+// Arizona Semantic Test
+const ARIZONA_IMAGE_CONTAINER = jQuery('<div/>', {
+    id: 'arizona-image-container',
+    class: 'arizona-container'
+})
+
+const ArizonaTopleftImage = jQuery('<img/>', {
+    id: 'arizona-topleftImage',
+    class: 'arizona-image'
+})
+
+const ArizonaTopRightImage = jQuery('<img/>', {
+    id: 'arizona-toprightImage',
+    class: 'arizona-image'
+})
+
+const ArizonaTargetImage = jQuery('<img/>', {
+    id: 'arizona-targetImage',
+    class: 'arizona-image'
+})
+
+const ArizonaBottomeLeftImage = jQuery('<img/>', {
+    id: 'arizona-botleftImage',
+    class: 'arizona-image'
+})
+
+const ArizonaBottomRightImage = jQuery('<img/>', {
+    id: 'arizona-botrightImage',
+    class: 'arizona-image'
+})
+
+ARIZONA_IMAGE_CONTAINER.append(ArizonaTopleftImage, ArizonaTopRightImage, ArizonaTargetImage, ArizonaBottomeLeftImage, ArizonaBottomRightImage)
+
+export { IMAGE_CONTAINER, FOUR_IMAGE_CONTAINER, topleftImage, toprightImage, botleftImage, botrightImage, TEXT_CRESP_CONTAINER,
+    ARIZONA_IMAGE_CONTAINER, ArizonaTopleftImage, ArizonaTopRightImage, ArizonaTargetImage, ArizonaBottomeLeftImage, ArizonaBottomRightImage }
