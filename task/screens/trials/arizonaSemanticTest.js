402e7584a62beaa4f3ebb6a01d7a43abd4bf71d7
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2025-05-15 17:32:28 -0700

    Adding variant trial class screen for Arizona Semantic Test

--- task/screens/trials/arizonaSemanticTest.js
+++ task/screens/trials/arizonaSemanticTest.js
@@ -1,10 +1,60 @@
 import Screen from "../base";
 import { TEXT_CONTAINER } from "../../../shared/components/textContainer";
 import { INSTRUCTION_BUTTON_CONTAINER } from "../../../shared/components/instructionButtons";
-import { IMAGE_CONTAINER } from "../../../shared/components/imageContainer";
+import { IMAGE_CONTAINER, topleftImage } from "../../../shared/components/imageContainer";
 
 class ArizonaSemanticTestTrialScreen extends Screen {
+    get components() {
+        return new Map([
+            [ARIZONA_IMAGE_CONTAINER, {}],
+            [TEXT_CONTAINER, {text: '+', addClass: 'base-text extra-large-text large-fixed-height'}]
+        ])
+    }
 
+    get clickHandlers() {
+        return {
+            topleftImage: (event) => this.proceedClickHandler(event, 'topleft'),
+            toprightImage: (event) => this.proceedClickHandler(event, 'topright'),
+            bottomleftImage: (event) => this.proceedClickHandler(event, 'bottomleft'),
+            bottomrightImage: (event) => this.proceedClickHandler(event, 'bottomright')
+        }
+    }
+
+    proceedClickHandler(event, location) {
+        event.stopPropagation()
+        clearTimeout(this.timeoutID)
+
+        this.orchestrator.currentTrial.responseTime = new Date()
+        this.orchestrator.currentTrial.ResponseLocation = location
+        this.orchestrator.currentTrial.Response = this.orchestrator.currentTrial.location[location]
+        const isPractice = this.orchestrator.currentTrial.TrialType === 'Practice'
+
+        if (!this.orchestrator.currentTrial.isCorrect() && isPractice) {
+            this.orchestrator.replay()
+        } else {
+            this.orchestrator.next()
+        }
+
+        TEXT_CONTAINER.show()
+    }
+
+    startTrial() {
+        // prepare the trial
+        // load the pictures into the containers
+        IMAGE_CONTAINER.hide()
+        topleftImage.attr('src', this.orchestrator.currentTrial.getTopLeft())
+        toprightImage.attr('src', this.orchestrator.currentTrial.getTopRight())
+        bottomleftImage.attr('src', this.orchestrator.currentTrial.getBotLeft())
+        bottomrightImage.attr('src', this.orchestrator.currentTrial.getBotRight())
+
+        setTimeout(() => {
+            IMAGE_CONTAINER.show()
+            this.orchestrator.currentTrial.startTime = new Date()
+            this.timeoutID = setTimeout(() => {
+                this.orchestrator.next()
+            }, this.orchestrator.currentTrial.timeToTimeout)
+        }, 1000)
+    }
 }
 
 class InstructionArizonaSemanticTest extends Screen {
