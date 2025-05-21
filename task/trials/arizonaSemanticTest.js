5d902f6a2f3b834f34223d522870a835acb56218
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2025-05-21 13:48:33 -0700

    adding variant trial class for written arizona semantic test

--- task/trials/arizonaSemanticTest.js
+++ task/trials/arizonaSemanticTest.js
@@ -85,4 +85,67 @@ class ArizonaSemanticTestTrial {
     }
 }
 
-export { ArizonaSemanticTestTrial }
+class WrittenArizonaSemanticTestTrial {
+    constructor(config) {
+        this.ItemNum = config.ItemNum
+        this.TrialType = config.TrialType
+        this.Procedure = config.Procedure
+
+        this.topleft = config.topleft
+        this.topright = config.topright
+        this.bottomleft = config.bottomleft
+        this.bottomright = config.bottomright
+
+        this.location = {
+            ['topleft']: config.topleft,
+            ['topright']: config.topright,
+            ['bottomleft']: config.bottomleft,
+            ['bottomright']: config.bottomright
+        }
+
+        this.targetimage = config.targetimage
+        this.CRESP = config.CRESP
+        this.TargetLocation = config.TargetLocation
+
+        this.Response = ''
+        this.ResponseLocation = ''
+
+        this.startTime = null
+        this.responseTime = null
+        this.TimedOut = null
+        this.TrialWasAdministered = 1
+    }
+
+    isCorrect() {
+        return this.ResponseLocation === this.TargetLocation
+    }
+
+    get columns() {
+        return [
+            ...Object.keys(this),
+            'Accuracy',
+            'Time',
+            'Date',
+            'TimedOut',
+            'RT'
+        ]
+    }
+
+    get Accuracy() {
+        return this.isCorrect() ? 1 : 0
+    }
+
+    get RT() {
+        return this.responseTime - this.startTime
+    }
+
+    get Time() {
+        return this.startTime.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', second: '2-digit', hourCycle: 'h24'})
+    }
+
+    get Date() {
+        return this.startTime.toLocaleDateString('en-US')
+    }
+}
+
+export { ArizonaSemanticTestTrial, WrittenArizonaSemanticTestTrial }
