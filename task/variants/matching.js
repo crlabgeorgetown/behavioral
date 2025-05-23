62a9d4cad194668a726806ea37c8d27e86aaaab8
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2025-05-22 17:25:33 -0700

    adding additional instructions to cross case letter match

--- task/variants/matching.js
+++ task/variants/matching.js
@@ -1,4 +1,4 @@
-import { InputDevice, ParticipantId, InstructionOne } from "../screens/instruction";
+import { InputDevice, ParticipantId, InstructionOne, InstructionTwo } from "../screens/instruction";
 import { LetsPractice } from "../screens/transition";
 import { CrossCaseLetterTrial } from "../trials/matching";
 import { CrossCaseLetterTrialScreen } from "../screens/trials/matching";
@@ -9,20 +9,21 @@ class CrossCaseLetter {
             ParticipantId,
             InputDevice,
             InstructionOne,
-            LetsPractice
-        ];
+            InstructionTwo,
+        ]
 
-        this.testNameShort = "CrossCaseLetter";
-        this.buildTestID = 155;
-        this.ePrimeTemplateID = 51;
-        this.fixationDuration = 500;
-        this.timeToTimeout = 5000;
+        this.testNameShort = "CrossCaseLetter"
+        this.buildTestID = 155
+        this.ePrimeTemplateID = 51
+        this.fixationDuration = 500
+        this.timeToTimeout = 5000
 
         this.InstructionOne = "You will see a letter.\n\nSelect the matching letter."
+        this.InstructionTwo = "We'll start with a few practice items before we begin."
 
-        this.trialClass = CrossCaseLetterTrial;
-        this.trialScreenClass = CrossCaseLetterTrialScreen;
+        this.trialClass = CrossCaseLetterTrial
+        this.trialScreenClass = CrossCaseLetterTrialScreen
     }
 }
 
-export { CrossCaseLetter };
+export { CrossCaseLetter }
